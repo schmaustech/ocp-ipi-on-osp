8dfc1fbe43795db509c749bdc604409e889549c9
Benjamin Schmaus <bschmaus@redhat.com>
2020-04-03 08:11:24 -0500

    Create rhsm-register.sh

---
+++ rhsm-register.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Register subscriptions
+sudo subscription-manager register
+sudo subscription-manager attach --pool=<pool id here>
+sudo subscription-manager repos --disable=*
+sudo subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms --enable=rhel-8-for-x86_64-appstream-rpms --enable=ansible-2-for-rhel-8-x86_64-rpms
+sudo subscription-manager repos --enable=openstack-15-tools-for-rhel-8-x86_64-rpms --enable=openstack-15-for-rhel-8-x86_64-rpms
+sudo yum -y install ipmitool libvirt jq libguestfs virt* qemu* bind-utils bind
+sudo yum -y upgrade
