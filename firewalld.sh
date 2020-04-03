f1ca163b628212f255d3194cd59578d02c0d3042
Benjamin Schmaus <bschmaus@redhat.com>
2020-04-03 08:09:41 -0500

    Create firewalld.sh

---
+++ firewalld.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Enable Firewalld
+sudo yum -y install firewalld 
+sudo systemctl --now enable firewalld
+sudo systemctl status firewalld
+sudo firewall-cmd --add-service=dns --permanent
+sudo firewall-cmd --add-service=http  --permanent
+sudo firewall-cmd --add-port=5000/tcp  --permanent
+sudo firewall-cmd --add-service=dhcp  --permanent
+sudo firewall-cmd --reload
