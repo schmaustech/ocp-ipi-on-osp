20312be7d6c78165d7f495b97276026b27ae3041
Benjamin Schmaus <bschmaus@redhat.com>
2020-04-06 10:36:45 -0500

    Update build-bridges.sh

--- build-bridges.sh
+++ build-bridges.sh
@@ -11,3 +11,6 @@ export MAIN_CONN="eth1"
 nmcli connection add ifname baremetal type bridge con-name baremetal
 nmcli con add type bridge-slave ifname "$MAIN_CONN" master baremetal
 nmcli con down "System $MAIN_CONN";pkill dhclient;dhclient baremetal
+nmcli connection modify baremetal ipv4.addresses 10.20.0.5/24 ipv4.method manual
+nmcli connection modify baremetal ipv4.gateway 10.20.0.1
+nmcli con down baremetal; nmcli con up baremetal
