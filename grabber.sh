8149b313c6120bf59f5dce9dbcce551ffd16ca78
Benjamin Schmaus <bschmaus@redhat.com>
2020-04-06 11:15:34 -0500

    Update grabber.sh

--- grabber.sh
+++ grabber.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#NODE=worker0
-for NODE in $( cat list )
+export OSP_PROJECT=smoketes-7df4-project
+for NODE in $( openstack --os-cloud=$OSP_PROJECT server list|egrep "worker|master"|cut -d\| -f3|sed 's/ //g' )
 do
  NAME=$(echo $NODE|sed 's/.$//')
  NUMBER=$(echo $NODE|sed 's/[^0-9]*//g')
- PXEMAC=$(openstack --os-cloud=msp-b910-project  port list --network schmaustech-pxe-network|grep $NAME|grep "\-$NUMBER\-"|cut -d\| -f 4|sed 's/ //g')
- IPMIPORT=$(openstack --os-cloud=msp-b910-project server show $NODE|grep port|cut -d\| -f3|cut -d, -f5|cut -d\' -f2)
+ PXEMAC=$(openstack --os-cloud=$OSP_PROJECT  port list --network schmaustech-pxe-network|grep $NAME|grep "\-$NUMBER\-"|cut -d\| -f 4|sed 's/ //g')
+ IPMIPORT=$(openstack --os-cloud=$OSP_PROJECT server show $NODE|grep port|cut -d\| -f3|cut -d, -f5|cut -d\' -f2)
  echo "$NODE $PXEMAC $IPMIPORT"
 done
+
