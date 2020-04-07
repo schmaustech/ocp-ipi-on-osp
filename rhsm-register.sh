b2672b830b6b761dfb3c18c909c5ae7da8daa50f
hgeaydem <hgeaydem@redhat.com>
2020-04-07 15:22:44 +0200

    initial commit, changing HTPASSWD in prep-node & adding poolid

--- rhsm-register.sh
+++ rhsm-register.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Register subscriptions
 sudo subscription-manager register
-sudo subscription-manager attach --pool=<pool id here>
+sudo subscription-manager attach --pool=8a85f99c6c8b9588016c8be0f1b50ec1
 sudo subscription-manager repos --disable=*
 sudo subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms --enable=rhel-8-for-x86_64-appstream-rpms --enable=ansible-2-for-rhel-8-x86_64-rpms
 sudo subscription-manager repos --enable=openstack-15-tools-for-rhel-8-x86_64-rpms --enable=openstack-15-for-rhel-8-x86_64-rpms
