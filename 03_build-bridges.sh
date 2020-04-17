1252e967c02603d459ae4250e27d410fac9317ad
Benjamin Schmaus <bschmaus@redhat.com>
2020-04-17 14:54:01 -0500

    Update 03_build-bridges.sh

--- 03_build-bridges.sh
+++ 03_build-bridges.sh
@@ -2,15 +2,15 @@
 # Build the bridges on provisioning node
 
 export PROV_CONN="eth0"
-nmcli connection add ifname provisioning type bridge con-name provisioning
-nmcli con add type bridge-slave ifname "$PROV_CONN" master provisioning
-nmcli connection modify provisioning ipv4.addresses 172.22.0.1/24 ipv4.method manual
-nmcli con down provisioning
-nmcli con up provisioning
+sudo nmcli connection add ifname provisioning type bridge con-name provisioning
+sudo nmcli con add type bridge-slave ifname "$PROV_CONN" master provisioning
+sudo nmcli connection modify provisioning ipv4.addresses 172.22.0.1/24 ipv4.method manual
+sudo nmcli con down provisioning
+sudo nmcli con up provisioning
 export MAIN_CONN="eth1"
-nmcli connection add ifname baremetal type bridge con-name baremetal
-nmcli con add type bridge-slave ifname "$MAIN_CONN" master baremetal
-nmcli con down "Wired connection 1";pkill dhclient;dhclient baremetal
-nmcli connection modify baremetal ipv4.addresses 10.20.0.5/24 ipv4.method manual
-nmcli connection modify baremetal ipv4.gateway 10.20.0.1
-nmcli con down baremetal; nmcli con up baremetal
+sudo nmcli connection add ifname baremetal type bridge con-name baremetal
+sudo nmcli con add type bridge-slave ifname "$MAIN_CONN" master baremetal
+sudo nmcli con down "Wired connection 1";sudo pkill dhclient;sudo dhclient baremetal
+sudo nmcli connection modify baremetal ipv4.addresses 10.20.0.5/24 ipv4.method manual
+sudo nmcli connection modify baremetal ipv4.gateway 10.20.0.1
+sudo nmcli con down baremetal; sudo nmcli con up baremetal
